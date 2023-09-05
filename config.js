cda97cf59820ba03e13c341fb72255c4529e4d3b
77xEfe <126316216+77xEfe@users.noreply.github.com>
2023-09-05 12:58:31 +0300

    MinecraftAfkBot
    Premium Sistemli Afk Bot

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "MTE0NTk5OTQ5MzQxNDc4MDkyOQ.GmPZ_3.uGvSECet6Rt6fE0sEw-hZFQVLVtCO4p8OdDVkg",
+    id: "1145999493414780929",
+    name: "PatronAFK",
+    premiumroleid: "1148509905079238677",
+    logkanalid: "1148549882710347827"
+}
